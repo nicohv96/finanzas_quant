f6ad506bfc9de32af4cbf21336ff1376aa59cf59
nicohv96 <57212697+nicohv96@users.noreply.github.com>
2024-05-21 16:56:14 -0300

    Actualizacion de la plataforma 21-05-2024

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const menuIcon = document.querySelector('.menu-icon');
 const burgerMenu = document.querySelector('.burger-menu');
 const overlay = document.querySelector('.overlay');
 const topButton = document.querySelector('.top-button');
+const main_1 = document.querySelector('#main_1');
+const main_2 = document.querySelector('#main_2');
+const main_3 = document.querySelector('#main_3');
 
 menuIcon.addEventListener('click', () => {
   burgerMenu.classList.toggle('active');
@@ -27,4 +30,7 @@ window.addEventListener('resize', () => {
 window.addEventListener("scroll", () => {
   header.classList.toggle("down",window.scrollY>0);
   topButton.classList.toggle("show",window.scrollY>0);
+  main_1.classList.add("flip-in-hor-bottom",window.scrollY>60);
+  main_2.classList.add("flip-in-hor-bottom",window.scrollY>60);
+  main_3.classList.add("flip-in-hor-bottom",window.scrollY>60);
 });
